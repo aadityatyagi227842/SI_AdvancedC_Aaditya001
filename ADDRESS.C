429b74852d868431ed0ac3ff0306708816ebf87a
Aaditya Tyagi <aadityatyagi227842@gmail.com>
2026-06-22 20:51:45 +0530

    Fetching the address of variable 'y'.

---
+++ ADDRESS.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x_add = 10003;
+    int size = 2;
+    printf("Base Address of X = %d\n", x_add);
+    printf("Size of each variable = %d bytes\n\n", size);
+    printf("CASE 1 : X -> Y -> Z\n");
+    printf("Address of X = %d\n", x_add);
+    printf("Address of Y = %d\n", x_add + size);
+    printf("Address of Z = %d\n\n", x_add + 2 * size);
+    printf("CASE 2 : Z -> Y -> X\n");
+    printf("Address of Z = %d\n", x_add - 2 * size);
+    printf("Address of Y = %d\n", x_add - size);
+    printf("Address of X = %d\n", x_add);
+    return 0;
+}
